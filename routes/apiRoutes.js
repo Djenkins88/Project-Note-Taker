f6ee84f2f8e16cd8831ac5d2f87a8044b98b1378
Dominque Jenkins <dominquejenkins14@gmail.com>
2021-07-28 20:56:31 -0500

    added some commits to code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -20,3 +20,51 @@ module.exports = function (app) {
         response.json(data);
     });
 }
+
+app.post("/api/notes", (request, response) => {
+
+    // Extracted new note from request body.  
+    const newNote = request.body;
+    
+    console.log("\n\nPOST request - New Note : " + JSON.stringify(newNote));
+
+    // Assigned unique id obtained from 'uuid' package
+    newNote.id = uuidv4();
+
+    // Read data from 'db.json' file
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+    // Pushed new note in notes file 'db.json'
+    data.push(newNote);
+
+    // Written notes data to 'db.json' file
+    fs.writeFileSync('./db/db.json', JSON.stringify(data));
+    
+    console.log("\nSuccessfully added new note to 'db.json' file!");
+
+    // Send response
+    response.json(data);
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+
+    // Fetched id to delete
+    let noteId = request.params.id.toString();
+    
+    console.log(`\n\nDELETE note request for noteId: ${noteId}`);
+
+    // Read data from 'db.json' file
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+    // filter data to get notes except the one to delete
+    const newData = data.filter( note => note.id.toString() !== noteId );
+
+    // Write new data to 'db.json' file
+    fs.writeFileSync('./db/db.json', JSON.stringify(newData));
+    
+    console.log(`\nSuccessfully deleted note with id : ${noteId}`);
+
+    // Send response
+    response.json(newData);
+});
+
